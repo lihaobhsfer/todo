c574534d06afbe0707815aa764e81c89585692e5
Hao Li <haoli2@andrew.cmu.edu>
2018-12-27 09:56:58 +0800

    initial commit of todo

---
+++ src/Components/TodoApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AddTodo from './AddTodo';
+import TodoList from './TodoList';
+import Footer from './Footer';
+
+import todos from '../store.js';
+
+const TodoApp = () => (
+    <div>
+        {/* several components */}
+        <AddTodo />
+        <TodoList todos={todos}/>
+        <Footer />
+    </div>
+);
+
+export default TodoApp;
